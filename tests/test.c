401fd3c4afa1d33a0a627b7947f9a53926fc9a28
K <kwailamchan@hotmail.com>
2017-09-21 13:09:56 +0800

    [gemm_tuned] refactor files.h. 	modified:   tests/test.c 	modified:   utils/filer.h

--- tests/test.c
+++ tests/test.c
@@ -4,28 +4,24 @@
 #include "timer.h"
 
 
-
 int main()
 {
-    float* abuff = malloc(256 * 2400 * 4);
-    float* bbuff = malloc(7676 * 2400 * 4);
-    float* cbuff = malloc(256 * 7676 * 4);
-    file_read(abuff, bbuff);
-    printf("Aabuff:%x  Bbbuff:%x Ccbuff:%x \r\n",abuff,bbuff,cbuff);
+    float * A = malloc(256 * 2400 * 4);
+    float * B = malloc(7676 * 2400 * 4);
+    float * C = malloc(256 * 7676 * 4);
+    file_read(A, 256*2400, "./data/demo_2nd_conv_A");
+    file_read(B, 7676*2400, "./data/demo_2nd_conv_B");
+    printf("A: %x  B: %x C: %x \r\n", A, B, C);
 
-    struct timespec start, finish;
-    double elapsed;
-    
     sub_pthread_init();
-
     double tic = timer();
-    sgemm_thread_nn(abuff, bbuff, cbuff, 256, 7676, 2400);
-    printf("sgemm_thread_nn elapsed time:%f cbuff[128*7676]:%f\r\n", timer() - tic, cbuff[128*7676]);
+    sgemm_thread_nn(A, B, C, 256, 7676, 2400);
+    printf("(gemm_nn) elapsed:%f, C[128*7676]: %f\r\n", timer() - tic, C[128*7676]);
     sub_pthread_exit();
 
-    free(abuff);
-    free(bbuff);
-    free(cbuff);
+    free(A);
+    free(B);
+    free(C);
     
     return 0;
 }
--- utils/filer.h
+++ utils/filer.h
@@ -1,23 +1,17 @@
 #ifndef FILER_H
 #define FILER_H
 
-void file_read(float * a, float * b)
+void file_read(float * A, int buffsize, char * filepath)
 {
-  int fd_a, fd_b;
+  int f;
 
-  if((fd_a = fopen("./data/demo_2nd_conv_A","rb")) ==-1)
+  if((f = fopen(filepath,"rb")) ==-1)
   {
-    printf("A creat file wrong!");
+    printf(stderr, "Error: File created error.\n");
   }
+  printf("[%s] Read size: %d \r\n",  filepath, fread(A, 4, buffsize, f));
 
-  if((fd_b = fopen("./data/demo_2nd_conv_B","rb")) ==-1)
-  {
-    printf("B creat file wrong!");
-  }
-  printf("A read size:%d \r\n",  fread(a, 4, 256 * 2400, fd_a));
-  printf("B read size:%d  \r\n", fread(b, 4, 7676 * 2400, fd_b));
-  close(fd_a);
-  close(fd_b);
+  close(f);
 }
 
 #endif
